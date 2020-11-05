b6f3ffa2398dfef4036bb630bb981079798c2498
ansonymendezsuarez@gmail.com <ansonymendezsuarez@gmail.com>
2020-11-04 18:30:35 -0600

    Backend ready

---
+++ Backend/DAL/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using Backend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.DAL
+{
+    class UnitOfWork<T> : IDisposable where T : class
+    {
+        private readonly BDContext context;
+        public IDALGeneric<T> genericDAL;
+
+        public UnitOfWork(BDContext _context)
+        {
+            context = _context;
+            genericDAL = new DALGenericImplementation<T>(context);
+        }
+
+        public bool Complete()
+        {
+            try
+            {
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                string msj = e.Message;
+                return false;
+            }
+
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
