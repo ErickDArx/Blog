2706ecba5570b1a6d41a172739a03763b6f6ba60
ansonymendezsuarez@gmail.com <ansonymendezsuarez@gmail.com>
2020-11-11 08:15:42 -0600

    vistas basicas e inicio de ldn

---
+++ Frontend/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Frontend.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
